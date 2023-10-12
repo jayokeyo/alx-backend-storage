d1f40708ed7c06072ae4490b1dd47252be4c0a5c
jayokeyo <jaykopiyo@gmail.com>
2023-10-12 20:45:53 +0000

    Added 0-uniq_users.sql

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 -- SQL script that creates a table users some requirements:
 
-CREATE TABLE users (
+CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL AUTO_INCREMENT,
 	email varchar(255) NOT NULL,
 	name varchar(255),
