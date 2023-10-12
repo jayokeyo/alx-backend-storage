d0c4c7508412a5528dec88228260bfc442e87f70
jayokeyo <jaykopiyo@gmail.com>
2023-10-12 20:54:05 +0000

    Added 1-country_users.sql

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS users (
 	id int NOT NULL AUTO_INCREMENT,
 	email varchar(255) NOT NULL,
 	name varchar(255),
-	country ENUM(US, CO, TN) NOT NULL DEFAULT "US",
+	country ENUM(US, CO, TN) DEFAULT 'US',
 	PRIMARY KEY(id),
 	UNIQUE(email)
 );
