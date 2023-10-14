24dbd2ce258e4118839228f149ed0687fe397d67
jayokeyo <jaykopiyo@gmail.com>
2023-10-14 13:37:27 +0000

    Added 10-div.sql

---
+++ 0x00-MySQL_Advanced/10-div.sql
@@ -0,0 +1,18 @@
+-- creates a function that divides two numbers
+-- returns 0 if the second number is equal to 0.
+DELIMITER $$ ;
+CREATE FUNCTION SafeDiv(
+	a INT,
+	b INT
+)
+RETURNS FLOAT
+DETERMINISTIC
+BEGIN
+	DECLARE result FLOAT;
+	IF b = 0 THEN
+		RETURN 0;
+        END IF;
+        SET result = (a * 1.0) / b;
+        RETURN result;
+END;$$
+DELIMITER ;
